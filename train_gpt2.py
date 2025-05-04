498bef4b2497631f7e768248ac9e7d1aa9197187
ClearrSky <2200013076@stu.pku.edu.cn>
2025-05-05 02:43:04 +0800

    [2200013076] add dataloader and optimizer

--- train_gpt2.py
+++ train_gpt2.py
@@ -150,6 +150,40 @@ class GPT(nn.Module): # GPT language model
         return model
 
 #---------------------------------------
+
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)  # 这里没有转移到GPU是因为数据集较小，不必要转移浪费GPU显存
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+    
+#---------------------------------------
+
 # Auto detect devices
 device = "cpu"
 if torch.cuda.is_available():
@@ -166,6 +200,21 @@ model = GPT(GPTConfig())
 model.eval()        # 将模型设置为推理/评估状态
 model.to(device)
 
+train_loader = DataLoaderLite(B=4, T=32)
+# optimization
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x,y = train_loader.next_batch()
+    x = x.to(device)
+    y = y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x,y)
+    loss.backward()
+    optimizer.step()
+    print("step: %d, loss: %f" % (i, loss.item()))
+
+import sys;sys.exit(0)
+
 # prefix tokens
 import tiktoken
 enc = tiktoken.get_encoding("gpt2")
@@ -174,6 +223,8 @@ tokens = torch.tensor(tokens,dtype=torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_seqeunces,1) # repeat(size)，按size在对应维度重复相应次数
 x = tokens.to(device)
 
+
+    
 # start generate
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
@@ -197,3 +248,4 @@ for i in range(num_return_seqeunces):
     tokens = x[i,:max_length].tolist()
     text = enc.decode(tokens)
     print(">",text)
+    
