f679f8ec8ad63191e9af57371b9df86e895e4c04
ClearrSky <2200013076@stu.pku.edu.cn>
2025-05-05 03:11:43 +0800

    [2200013076] Add proper weight init

--- train_gpt2.py
+++ train_gpt2.py
@@ -14,6 +14,7 @@ class CasualSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output matrix
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCLAE_INIT = 1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size))
@@ -82,6 +83,21 @@ class GPT(nn.Module): # GPT language model
 
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
+        
+        # init params
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)    # 没有修改为pe std=0.01，因为差不多
+            
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
         B, T = idx.size()
